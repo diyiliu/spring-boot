d4aff2ea8f3b7ce7d6e0e77c3adb5152785ca9a3
DIYILIU <572772828@qq.com>
2017-10-24 15:20:29 +0800

    Initial commit

---
+++ src/main/java/com/diyiliu/AppApplication.java
@@ -0,0 +1,19 @@
+package com.diyiliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: AppApplication
+ * Author: DIYILIU
+ * Update: 2017-10-24 14:01
+ */
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
---
+++ src/main/java/com/diyiliu/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.diyiliu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Description: HelloController
+ * Author: DIYILIU
+ * Update: 2017-10-24 14:03
+ */
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+
+        return "Hello, world!";
+    }
+
+    @RequestMapping("/index")
+    public String index(@RequestParam(defaultValue = "world") String name, Model model){
+        model.addAttribute("message", "你好, " + name);
+
+        return "index";
+    }
+
+    @RequestMapping("/search")
+    public String search(){
+
+        return "search";
+    }
+}
