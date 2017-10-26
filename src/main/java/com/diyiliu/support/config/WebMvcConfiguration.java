f50b98f1ba15b8b5faf468f91ebaf73dfd409512
DIYILIU <572772828@qq.com>
2017-10-26 09:54:41 +0800

    设置多国语言，国际化

---
+++ src/main/java/com/diyiliu/support/config/WebMvcConfiguration.java
@@ -0,0 +1,45 @@
+package com.diyiliu.support.config;
+
+import com.diyiliu.support.format.CNLocalDateFormatter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.time.LocalDate;
+
+/**
+ * Description: WebMvcConfiguration
+ * Author: DIYILIU
+ * Update: 2017-10-25 08:57
+ */
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatterForFieldType(LocalDate.class, new CNLocalDateFormatter());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
--- src/main/java/com/diyiliu/web/controller/ProfileController.java
+++ src/main/java/com/diyiliu/web/controller/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -21,13 +22,13 @@ import java.util.Locale;
 public class ProfileController {
 
     @RequestMapping("/profile")
-    public String displayProfile(ProfileForm profileForm){
+    public String displayProfile(ProfileForm profileForm) {
 
         return "profile/profilePage";
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult){
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "profile/profilePage";
         }
@@ -37,9 +38,22 @@ public class ProfileController {
         return "redirect:/profile";
     }
 
+    @RequestMapping(value = "/profile", params = {"addTaste"})
+    public String addRow(ProfileForm profileForm) {
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", params = {"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+        Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profilePage";
+    }
+
 
     @ModelAttribute("dateFormat")
-    public String localeFormat(Locale locale){
+    public String localeFormat(Locale locale) {
 
         return CNLocalDateFormatter.getPattern(locale);
     }
